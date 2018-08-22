2d5ddaebabbed9c5cf9efff543a96fd12fed443e
knif3r2 <willaveryvy@gmail.com>
2018-08-22 18:30:51 -0400

    zxczxc

--- OrakeBot/Form1.Designer.cs
+++ OrakeBot/Form1.Designer.cs
@@ -28,12 +28,94 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.p_menubar = new System.Windows.Forms.Panel();
+            this.p_minimize = new System.Windows.Forms.Panel();
+            this.p_exitbutton = new System.Windows.Forms.Panel();
+            this.p_menubar.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(293, 289);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(293, 318);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // p_menubar
+            // 
+            this.p_menubar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.p_menubar.Controls.Add(this.p_minimize);
+            this.p_menubar.Controls.Add(this.p_exitbutton);
+            this.p_menubar.Dock = System.Windows.Forms.DockStyle.Top;
+            this.p_menubar.Location = new System.Drawing.Point(0, 0);
+            this.p_menubar.Name = "p_menubar";
+            this.p_menubar.Size = new System.Drawing.Size(380, 21);
+            this.p_menubar.TabIndex = 2;
+            this.p_menubar.MouseDown += new System.Windows.Forms.MouseEventHandler(this.p_menubar_MouseDown);
+            // 
+            // p_minimize
+            // 
+            this.p_minimize.Dock = System.Windows.Forms.DockStyle.Right;
+            this.p_minimize.Location = new System.Drawing.Point(296, 0);
+            this.p_minimize.Name = "p_minimize";
+            this.p_minimize.Size = new System.Drawing.Size(42, 21);
+            this.p_minimize.TabIndex = 4;
+            // 
+            // p_exitbutton
+            // 
+            this.p_exitbutton.BackgroundImage = global::OrakeBot.Properties.Resources.ExitButton;
+            this.p_exitbutton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.p_exitbutton.Dock = System.Windows.Forms.DockStyle.Right;
+            this.p_exitbutton.Location = new System.Drawing.Point(338, 0);
+            this.p_exitbutton.Name = "p_exitbutton";
+            this.p_exitbutton.Size = new System.Drawing.Size(42, 21);
+            this.p_exitbutton.TabIndex = 3;
+            this.p_exitbutton.MouseClick += new System.Windows.Forms.MouseEventHandler(this.p_exitbutton_MouseClick);
+            this.p_exitbutton.MouseDown += new System.Windows.Forms.MouseEventHandler(this.p_exitbutton_MouseDown);
+            this.p_exitbutton.MouseEnter += new System.EventHandler(this.p_exitbutton_MouseEnter);
+            this.p_exitbutton.MouseLeave += new System.EventHandler(this.p_exitbutton_MouseLeave);
+            this.p_exitbutton.MouseUp += new System.Windows.Forms.MouseEventHandler(this.p_exitbutton_MouseUp);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.Color.SlateGray;
+            this.ClientSize = new System.Drawing.Size(380, 353);
+            this.Controls.Add(this.p_menubar);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "OrakeBot";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.p_menubar.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Panel p_menubar;
+        private System.Windows.Forms.Panel p_minimize;
+        private System.Windows.Forms.Panel p_exitbutton;
     }
 }
 
--- OrakeBot/Form1.cs
+++ OrakeBot/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,9 +13,76 @@ namespace OrakeBot
 {
     public partial class Form1 : Form
     {
+        public const int WM_NCLBUTTONDOWN = 0xA1;
+        private const int WM_NCHITTEST = 0x84;
+        private const int HTCLIENT = 0x1;
+        private const int HTCAPTION = 0x2;
+
+        [DllImport("User32.dll")]
+        public static extern bool ReleaseCapture();
+        [DllImport("User32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+        protected override void WndProc(ref Message m)
+        {
+            switch (m.Msg)
+            {
+                case WM_NCHITTEST:
+                    base.WndProc(ref m);
+                    if ((int)m.Result == HTCLIENT)
+                    {
+                        m.Result = (IntPtr)HTCAPTION;
+                    }
+                    return;
+            }
+
+            base.WndProc(ref m);
+        }
+
+        private void p_menubar_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                ReleaseCapture();
+                SendMessage(Handle, WM_NCLBUTTONDOWN, HTCAPTION, 0);
+            }
+        }
+
+        private void p_exitbutton_MouseDown(object sender, MouseEventArgs e)
+        {
+            this.p_exitbutton.BackgroundImage = OrakeBot.Properties.Resources.ExitButton_Pressed;
+        }
+
+        private void p_exitbutton_MouseUp(object sender, MouseEventArgs e)
+        {
+            this.p_exitbutton.BackgroundImage = OrakeBot.Properties.Resources.ExitButton;
+        }
+
+        private void p_exitbutton_MouseEnter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void p_exitbutton_MouseLeave(object sender, EventArgs e)
+        {
+
+        }
+
+        private void p_exitbutton_MouseClick(object sender, MouseEventArgs e)
+        {
+            Environment.Exit(0);
         }
     }
 }
